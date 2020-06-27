c76f5184adcf05df6628f547b4b3be5b61734bcb
furupon <furupon2008@gmail.com>
2020-06-27 16:05:45 +0900

    Initial Commit

---
+++ LandScope3rd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandScope3rd
+//
+//  Created by 古川とおる on 2020/06/27.
+//  Copyright © 2020 古川とおる. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
